d79d73be3fd98a805d21b36439b36df9887e505e
Davide Corsi <davide.corsi@par-tec.it>
2021-08-09 15:31:36 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	public static void main(String[] args) {
+		int[] n;
+		n = new int[10];
+		n[0] = 5;
+		int [] n1 = {3, 6, 7, 9, 10, 1, 4, 6, 15, 2};
+		
+		// Utilizzando il for
+		for(int i = 0; i < n1.length; i++) {
+			System.out.println(n1[i]);
+		}
+		
+		// Utilizzando il for each
+		for(int i: n1) {
+			System.out.println(i);
+		}
+	}
+}
